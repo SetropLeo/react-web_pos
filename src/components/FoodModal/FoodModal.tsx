8310d7d972abf02987e6259ce8878433c8162386
LeonardoPortes <leol_portes@hotmail.com>
2022-04-26 23:09:04 -0300

    Food Modal component adjustments

--- src/components/FoodModal/FoodModal.tsx
+++ src/components/FoodModal/FoodModal.tsx
@@ -8,13 +8,19 @@ const FoodModal = ({ modalFood, closeModal, addFoodToCart }: any) => {
   const [quantity, setQuantity] = React.useState<number>(1);
 
 
-  function addToCart() {
+  function onClickAddToCart() {
     addFoodToCart(modalFood, quantity);
     closeModal();
+    setQuantity(1);
+  }
+
+  function onClickCloseModal() {
+    closeModal();
+    setQuantity(1);
   }
 
   return (
-    <div className={`foodModal-container ${modalFood !== '' ? 'active' : 'hidden'}`} onClick={() => closeModal()}>
+    <div className={`foodModal-container ${modalFood !== '' ? 'active' : 'hidden'}`} onClick={() => onClickCloseModal()}>
       <div className='foodModal-content' onClick={(event) => event.stopPropagation()}>
         <div className='foodModal-left'>
           <img src={modalFood?.image} alt="" />
@@ -23,15 +29,15 @@ const FoodModal = ({ modalFood, closeModal, addFoodToCart }: any) => {
           <div className='foodModal-right-content'>
             <h1 className='foodModal-name'>{modalFood?.name}</h1>
             <h2 className='foodModal-description'>{modalFood?.description}</h2>
-            <p className='foodModal-price'>${modalFood?.price * quantity}</p>
+            <p className='foodModal-price'>${(modalFood?.price * quantity).toFixed(2)}</p>
             <div className="productAmount-subcontainer">
               <p className="minus-sign" onClick={() => { if (quantity > 1) setQuantity(quantity - 1) }}>-</p>
               <p className="productAmount">{quantity}</p>
               <p className="plus-sign" onClick={() => setQuantity(quantity + 1)}>+</p>
             </div>
             <div className='addItems-container'>
-              <button className='cancel-button' onClick={() => closeModal()}>Cancel</button>
-              <button className='addToCart-button' onClick={() => addToCart()}>Add to cart +</button>
+              <button className='cancel-button' onClick={() => onClickCloseModal()}>Cancel</button>
+              <button className='addToCart-button' onClick={() => onClickAddToCart()}>Add to cart +</button>
             </div>
             <h3 onClick={() => closeModal()}>CLOSE BUTTON</h3>
           </div>
