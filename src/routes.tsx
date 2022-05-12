9527b369299596682fafd026d0d182f69d3b09b7
LeonardoPortes <leol_portes@hotmail.com>
2022-05-11 21:13:09 -0300

    Clear routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import CartElement from './components/CartSideBar/CartElement/CartElement';
-import OrderMenu from './components/CartSideBar/CartMenu/CartMenu';
-import ProductCard from './components/FoodShelf/FoodCard/FoodCard';
-import SideBar from './components/SideBar/SideBar';
+import OrderElement from './components/OrderListSideBar/OrderElement/OrderElement';
 import DishesCatalog from './pages/Catalog/Catalog';
 import Stock from './pages/Stock';
 import store from './store/store';
@@ -16,10 +13,6 @@ const AppRoutes: React.FC = () => {
         <Route path="/" element={<Navigate to="/catalogo" />} />
         <Route path="catalogo" element={<DishesCatalog />} />
         <Route path="estoque" element={<Stock />} />
-        <Route path="productcard" element={<ProductCard />} />
-        <Route path="sidebar" element={<SideBar />} />
-        <Route path="ordermenu" element={<OrderMenu />} />
-        <Route path="CartElement" element={<CartElement />} />
       </Routes>
     </Provider>
   );
