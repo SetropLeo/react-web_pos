23069bacac0ad9296802c653799181c462a9a948
LeonardoPortes <leol_portes@hotmail.com>
2022-05-11 21:14:22 -0300

    Change OrderElement main folder and create structure to show food order list

---
+++ src/components/OrderListSideBar/OrderElement/OrderElement.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Food, Order } from '../../../models';
+import store from '../../../store/store';
+import './OrderElement.css'
+
+const OrderElement = () => {
+  return (
+    <div className='orderList-container'>
+      {store.getState().orderMenuOption.orderList.map((order: Order) => {
+        return (
+          <div key={order.id + '-order'} className='individualOrder-container'>
+            {order.list.map((item: Food, index: number) => {
+              return (
+                <div key={index} className='individualFood-container'>
+                  <div className='individualFood-image'>
+                    <img src={item.image} alt="food" />
+                    <p className='food'></p>
+                  </div>
+                  <div className='individualFood-infos'>
+                    <p className='food-name'>{item.name}</p>
+                    <p className='food-quantity'>{item.quantity}</p>
+                    <p className='food-price'>${item.price}</p>
+                  </div>
+                </div>
+              )
+            })}
+            <div className='totalPrice-container'>
+              <p className='total-price-title'>Total Price: </p>
+              <p className='total-price-value'>${order.totalPrice}</p>
+            </div>
+          </div>
+        )
+      }
+      )}
+    </div>
+  )
+}
+
+export default OrderElement
