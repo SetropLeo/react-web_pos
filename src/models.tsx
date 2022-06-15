5e64376f7b096ee008a76db4f0ae1d8516b72e53
LeonardoPortes <leol_portes@hotmail.com>
2022-06-15 00:21:03 -0300

    Temporary models

--- src/models.tsx
+++ src/models.tsx
@@ -12,3 +12,31 @@ export type Order = {
   list: Food[];
   totalPrice: number;
 }
+
+export type Food2 = {
+  category: Category;
+  category_id: string;
+  created_at: Date
+  deleted_at: null
+  description: string;
+  id: string;
+  image: string;
+  name: string;
+  price: number
+  updated_at: string;
+}
+
+export type Category = {
+  id: string, 
+  created_at: Date, 
+  updated_at: Date, 
+  deleted_at: null, 
+  name: string
+}
+
+export type OrderDetail = {
+  id: string;
+  quantity: number;
+  food_id: string;
+  order_id: number;
+}
